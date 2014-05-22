82b64ac62d61389360c04531c57b1c77d0b4cfe7
Matija Matija <matija.matija@outlook.com>
2014-05-22 02:10:55 +0200

    Rename to JATWA

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,13 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  let(:base_title) { "JATWA" }
   subject {page}
 
   describe "Home page" do
   	before {visit root_path}
 
-    it {should have_content('Sample App')}
+    it {should have_content('JATWA')}
     it {should have_title(full_title(''))}
     it {should_not have_title('| Home')}
   end
